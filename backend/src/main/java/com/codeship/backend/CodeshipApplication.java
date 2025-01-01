193b9a6e3a2da03d2b8d5ad6def337bf7640c7f8
Young <yi5oyu@gmail.com>
2025-01-01 19:04:24 +0900

    프로젝트 생성

---
+++ backend/src/main/java/com/codeship/backend/CodeshipApplication.java
@@ -0,0 +1,13 @@
+package com.codeship.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeshipApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/codeship/backend/CodeshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeship.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
