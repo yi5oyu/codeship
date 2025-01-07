aec56be85f2a74c8e56e3bee76fba7157a6332f8
Young <yi5oyu@gmail.com>
2025-01-08 03:52:13 +0900

    ck

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <>
-      <div></div>
+      <div>
+        <h1>Hello World</h1>
+      </div>
     </>
   );
 }
